59da8516fbede5ca447c9f35aa38dad736800f64
DuyetQB <dinhsyduyet@gmail.com>
2021-02-07 09:05:15 +0700

    new commitcommit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import { ChatEngine } from 'react-chat-engine';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChatEngine
+    height='100vh'
+    userName='Sĩ Duyệt'
+    userSecret='12341234'
+    projectID='be002709-e468-4da9-a3be-05e9841b9c03'
+  />
   );
 }
 
